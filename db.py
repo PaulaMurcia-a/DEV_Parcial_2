98f34c05462550c27879a3f031e87db1ac3465f3
javiercito13 <jsahumada68@ucatolica.edu.co>
2026-05-07 14:37:36 -0500

    [ADD] BASE DE DATOS

--- db.py
+++ db.py
@@ -1,21 +1,25 @@
 import os
-from sqlmodel import Session, create_engine, SQLModel
-from fastapi import FastAPI, Depends
 from typing import Annotated
 
+from dotenv import load_dotenv
+from fastapi import Depends
+from sqlmodel import Session, SQLModel, create_engine
 
-engine = create_engine()
+load_dotenv()
 
-def create_all_tables(app: FastAPI):
-    if os.getenv("ENV") == "dev":
-        SQLModel.metadata.create_all(engine)
-    yield
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
 
-def get_session()->Session:
+def create_all_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
     with Session(engine) as session:
         yield session
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
-
